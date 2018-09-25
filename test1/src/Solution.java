ac29a97bf3edd932ce9c19398c10efdcf309eda1
Roma Frozzen <frozzen256@mail.ru>
2018-09-25 08:55:28 +0700

    Создание задач

---
+++ test1/src/Solution.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class Solution {
+
+    // Complete the staircase function below.
+    static void staircase(int n) {
+       }
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        staircase(n);
