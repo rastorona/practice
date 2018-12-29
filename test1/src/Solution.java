e5c2062da2f61aef9d24ac7b6de49b3a1054f029
VareHka <43487708+VareHka@users.noreply.github.com>
2018-12-29 19:18:06 +0700

    Laba3_2 Dardaev Ilya (#32)

--- test1/src/Solution.java
+++ test1/src/Solution.java
@@ -1,36 +1,50 @@
-import java.util.*;
+import  java.util.*;
+import java.lang.*;
 
-public class Solution {
+public class Solution<MyObject> {
 	/*
 	 *  Дано n - число уровней. Построить треугольник из символов #.
 	 *  Пример для n = 4:
-	 * 	  #
-	 *	 ##
-	 *  ###
-	 * ####
+	 *	   #
+	 *	  ##
+	 *   ###
+	 *  ####
 	 */
 
-	static void func1(int n) {
-		int k = n;
-		String o = "";
+	static class Wic
 
-		for (int i = 1; i <= n; i++) {
-			k = k - 1;
-			o = o + "#";
-			for (int j = 1; j <= k; j++) {
-				System.out.print(" ");
+	{
+		private int n;
+		public void SetN ( int a){
+			n = a;
+		}
+
+		public void func1 () {
+			int k = n;
+			String o = "";
+
+			for (int i = 1; i <= n; i++) {
+				k = k - 1;
+				o = o + "#";
+				for (int j = 1; j <= k; j++) {
+					System.out.print(" ");
+				}
+				System.out.print(o);
+				System.out.println();
 			}
-			System.out.print(o);
-			System.out.println();
 		}
 	}
 
+
+
 	private static final Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
+	public static void main (String[]args){
 		int n = scanner.nextInt();
 		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-		func1(n);
+		Wic MyObject = new Wic();
+		MyObject.SetN(n);
+		Thread myTr = new Thread(()-> MyObject.func1());
+		myTr.start();
 	}
 }
